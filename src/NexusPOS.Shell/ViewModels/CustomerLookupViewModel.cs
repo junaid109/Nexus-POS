fcea12e9e637c389e9e16e723fea4bb5125f5b52
Junaid Malik <junmalik109@gmail.com>
2026-01-17 15:11:52 +0000

    Feat: Add Customer Lookup Screen

---
+++ src/NexusPOS.Shell/ViewModels/CustomerLookupViewModel.cs
@@ -0,0 +1,33 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NexusPOS.Shell.Interfaces;
+using NexusPOS.Shell.ViewModels;
+
+namespace NexusPOS.Shell.ViewModels
+{
+    public partial class CustomerLookupViewModel : ObservableObject
+    {
+        private readonly INavigationService _navigationService;
+
+        [ObservableProperty]
+        private string customerIdentifier;
+
+        public CustomerLookupViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        [RelayCommand]
+        public void Search()
+        {
+            // TODO: Implement lookup logic
+             System.Diagnostics.Debug.WriteLine($"Searching for: {CustomerIdentifier}");
+        }
+
+        [RelayCommand]
+        public void GoBack()
+        {
+            _navigationService.NavigateTo<HelloViewModel>();
+        }
+    }
+}
