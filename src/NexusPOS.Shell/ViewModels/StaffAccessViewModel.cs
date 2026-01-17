b15e7fcb46c147e1a77469f21ef6c8c4992c1eee
Junaid Malik <junmalik109@gmail.com>
2026-01-17 15:11:39 +0000

    Feat: Add Staff Access Login Screen

---
+++ src/NexusPOS.Shell/ViewModels/StaffAccessViewModel.cs
@@ -0,0 +1,46 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NexusPOS.Shell.Interfaces;
+using NexusPOS.Shell.ViewModels;
+
+namespace NexusPOS.Shell.ViewModels
+{
+    public partial class StaffAccessViewModel : ObservableObject
+    {
+        private readonly INavigationService _navigationService;
+
+        [ObservableProperty]
+        private string staffId;
+
+        [ObservableProperty]
+        private string pinCode;
+
+        [ObservableProperty]
+        private string errorMessage;
+
+        public StaffAccessViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        [RelayCommand]
+        public void Login()
+        {
+            if (StaffId == "admin" && PinCode == "1234")
+            {
+                 // Create a StaffDashboardViewModel later
+                 ErrorMessage = "Login Successful (Dashboard not implemented)";
+            }
+            else
+            {
+                ErrorMessage = "Invalid Staff ID or PIN";
+            }
+        }
+
+        [RelayCommand]
+        public void Cancel()
+        {
+            _navigationService.NavigateTo<HelloViewModel>();
+        }
+    }
+}
